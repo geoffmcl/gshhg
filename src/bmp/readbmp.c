eac2238c84072101c542a347624325229f022a6b
Geoff McLane <ubuntu@geoffair.info>
2015-04-07 20:01:42 +0200

    name changed from endian.h to endianness.h

--- src/bmp/readbmp.c
+++ src/bmp/readbmp.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "bmptypes.h"
-#include "endian.h"
+#include "endianness.h"
 #include "readbmp.h"
 
 /*****************************************************************************
