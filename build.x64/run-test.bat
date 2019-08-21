45797dff33b2edf3229fadfff1cfcc42a0a44755
Geoff McLane <ubuntu@geoffair.info>
2019-08-21 21:07:28 +0200

    Change tests for add gray bounding box to xg output

--- build.x64/run-test.bat
+++ build.x64/run-test.bat
@@ -1,15 +1,31 @@
 @setlocal
+@REM For ????
 @set MINLAT=-18
 @set MINLON=-150
 @set MAXLAT=-17
 @set MAXLON=-149
 
-@set TMPXG=tempbbox.xg
-@set TMPGSHHG=D:\gshhg\bin\gshhs_f.b
+@REM For YSSY-LFPG
+@set MINLAT=-35
+@set MINLON=2
+@set MAXLAT=50
+@set MAXLON=152
+
+@REM for VHHH LFPG
+@set MINLAT=22.31
+@set MAXLAT=49.01
+@set MINLON=2.55
+@set MAXLON=113.92
+
+@set TMPXG=temp-VH-PG-c.xg
+@REM set TMPXG=temp-VH-PG-f.xg
+@REM set TMPXG=temp-Y-Lb-f.xg
+@set TMPGSHHG=D:\gshhg\bin\gshhs_c.b
+@REM set TMPGSHHG=D:\gshhg\bin\gshhs_f.b
 @REM set TMPGSHHG=D:\gshhg\bin\gshhs_i.b
 @REM can get nothing...
 @REM set TMPGSHHG=D:\gshhg\bin\wdb_rivers_f.b
-@REM set TMPGSHHG=D:\gshhg\bin\wdb_borders_f.b
+@rem set TMPGSHHG=D:\gshhg\bin\wdb_borders_f.b
 
 @if NOT EXIST %TMPGSHHG% goto NOGSHHG
 @set TMPEXE=Release\gshhg.exe
