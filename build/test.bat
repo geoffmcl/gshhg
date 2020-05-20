7d06cb465b3288690389c67699697767c47bec43
Geoff McLane <ubuntu@geoffair.info>
2020-05-20 20:28:35 +0200

    Add updexe.bat, and mod test.bat

--- build/test.bat
+++ build/test.bat
@@ -4,8 +4,15 @@
 @echo Can NOT find %TMPEXE%! *** FIX ME ***
 @exit /b 1
 )
+@set TMPDATA=D:\DATA\GSHHG\gshhs_f.b
+@if NOT EXIST %TMPDATA% (
+@echo Can NOT find %TMPDATA%! *** FIX ME ***
+@exit /b 1
+)
 
-%TMPEXE% -b -30,63,-20,70 D:\DATA\GSHHG\gshhs_f.b -x BIKF.xg
 
-%TMPEXE% -b -30,63,-10,70 D:\DATA\GSHHG\gshhs_f.b -x BIKF2.xg
+
+%TMPEXE% -b -30,63,-20,70 %TMPDATA% -x BIKF.xg
+
+%TMPEXE% -b -30,63,-10,70 %TMPDATA% -x BIKF2.xg
 
